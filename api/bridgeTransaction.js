8230da49c2c48fca5f6e6a2af3d097a694f14d8a
bholagabbar <dev.shreyans96@gmail.com>
2022-05-10 15:01:08 -0400

    fix null fields

--- api/bridgeTransaction.js
+++ api/bridgeTransaction.js
@@ -10,17 +10,15 @@ export class BridgeTransaction {
         this.chainIdTo = args.toChainId;
         this.chainIdFrom = args.fromChainId;
         this.sentValue = args.sentValue;
+        this.receivedValue = args.receivedValue;
         this.sentTokenAddress = args.sentTokenAddress;
         this.sentTokenSymbol = args.sentTokenSymbol;
-        this.sentTime = args.sentTime;
         this.receivedTokenAddress = args.receivedTokenAddress;
         this.receivedTokenSymbol = args.receivedTokenSymbol;
+        this.sentTime = args.sentTime;
         this.receivedTime = args.receivedTime;
         this.kappa = args.kappa;
         this.pending = args.pending;
         this.swapSuccess = args.swapSuccess;
-
-        this.sentValueUSD = args.sentValueFormatted ? Decimal128(args.sent_value_formatted.toString()).toString() : null;
-        this.receivedValueUSD = args.sentValueFormatted ? Decimal128(args.received_value_formatted.toString()).toString() : null;
     }
 }
