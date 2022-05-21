0114950cd19f59b01e702c83ddf8eb315a206332
caesar <caesar0x@users.noreply.github.com>
2022-05-20 22:57:29 -0400

    fix sdk utils to be simpler, annotate graphql so syntax highlighting works properly

--- api/utils/sdkUtils.js
+++ api/utils/sdkUtils.js
@@ -1,26 +1,27 @@
+import _ from 'lodash'
 import {BaseToken, Tokens, ChainId} from "@synapseprotocol/sdk"
 
-let ADDRESS_SYMBOL_CACHE = {}
 
+let INVERTED_CHAIN_ID = {}
+for (const [chainName, chainId] of _.entries(ChainId)) {
+    INVERTED_CHAIN_ID[chainId] = chainName
+}
 export function getNetworkNameFromId(id) {
-    for (let key of Object.keys(ChainId)) {
-        if (id === ChainId[key]) {
-            return key;
+    return INVERTED_CHAIN_ID[id]
+}
+
+
+let ADDRESS_SYMBOL_MAP = {}
+for (const chainId of _.values(ChainId)) {
+    ADDRESS_SYMBOL_MAP[chainId] = {}
+    for (const [symbol, token] of _.entries(Tokens)) {
+        if (Tokens[symbol] instanceof BaseToken) {
+            const lowerAddress = Tokens[symbol].addresses[chainId]?.toLowerCase()
+            ADDRESS_SYMBOL_MAP[chainId][lowerAddress] = symbol
         }
     }
 }
 
 export function getTokenSymbolFromAddress(chainId, address) {
-    if (address in ADDRESS_SYMBOL_CACHE) {
-        return ADDRESS_SYMBOL_CACHE[address];
-    }
-
-    for (let key of Object.keys(Tokens)) {
-        if (Tokens[key] instanceof BaseToken) {
-            if (Tokens[key].addresses[chainId] &&
-                Tokens[key].addresses[chainId].toLowerCase() === address.toLowerCase()) {
-                return ADDRESS_SYMBOL_CACHE[address] = key;
-            }
-        }
-    }
+    return ADDRESS_SYMBOL_MAP[chainId][address.toLowerCase()]
 }
